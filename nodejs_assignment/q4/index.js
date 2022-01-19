421dda57d0c46fa5770a3a89df1b543970d5e197
sachin-14800 <sachin.14800@gmail.com>
2022-01-19 14:34:35 +0530

    images are downloaded using the download module

--- nodejs_assignment/q4/index.js
+++ nodejs_assignment/q4/index.js
@@ -1,13 +1,13 @@
 const fs=require('fs');
 const async=require('async');
 const path=require('path');
-
+const download=require('download');
 const arr=[
-    "http://sousmonarbre.com/qphj/bd963843d2239ed78aa6f7b0a36b537d/qdp/shapely-table-mat-design-office-bay-decoration-mes-at-work-decorating-ideas-office-decoration-mes-design-ideas-cream-wall-paint-decoration-messroom-wooden-laminate-ing-tosca-color__office-decorating-ideas.jpg",
+    "https://cdn.vox-cdn.com/thumbor/SXZUSV2LUllOzJmtOkk0vSBkveQ=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19662507/MV5BMDQ3ZWNmYzYtMzY2MS00MjMxLWE4MjAtNDdkOTQ5NzQ5OTUyXkEyXkFqcGdeQXVyNTQxOTM1NTc_._V1_SY1000_CR0_0_1502_1000_AL_.jpg",
     "https://doodleart.redbull.com/assets/managed/entries/processed/sm/367010617181759_36211000.jpg",
     "https://www.justcolor.net/wp-content/uploads/sites/1/nggallery/doodle-art-doodling/coloring-page-adults-doodle-art-rachel.jpg",
     "https://i.pinimg.com/originals/e5/55/a3/e555a39ca5457a079a9bcce59f61f8d5.jpg",
-    "http://canhotopazelite.info/wp-content/uploads/2018/08/office-bay-decoration-themes-with-office-bay-decoration-themes-elegant-yet-fun-office-bay-decoration-14.jpg",
+    "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b6037c93472227.5e6769f0d9af7.png",
     "https://i.pinimg.com/originals/ef/4c/91/ef4c91fb73e61e19211a0589187ccaa6.jpg",
     "https://static.vecteezy.com/system/resources/previews/000/107/464/non_2x/huge-doodle-vector-pack.jpg",
     "https://i.ytimg.com/vi/O5u1apUkYV0/maxresdefault.jpg",
@@ -20,20 +20,21 @@ if(!fs.existsSync(dir))
     fs.mkdirSync(dir);
     async.parallel([
        function(){
-        arr.forEach((element)=>{
-            if(fs.readdirSync(dir).length>=5)
+        arr.forEach(async (element)=>{
+            if(fs.readdirSync(dir).length>=4)
             {
-                num++;
-                dir=`folder${num}`;
+                while(fs.readdirSync(dir).length>4)
+                {   num++;
+                    dir=`folder${num}`;
+                    if(!fs.existsSync(dir))
+                        fs.mkdirSync(dir);
+                }
             }
-            if(!fs.existsSync(dir))
-            fs.mkdirSync(dir);
-            fs.writeFileSync(`${dir}/image${arr.indexOf(element)}.jpg`,element,'base64',(err)=>{
-                if(err)
-                console.log(err);
-            });
             
-        })
+
+            download(element).pipe(fs.createWriteStream(`${dir}/image${arr.indexOf(element)+1}.jpg`)); 
+            
+        });
        } 
     ]).then(results=>{
         console.log(results);
