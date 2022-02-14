56fddc63b9ca2da01fcc1da6054bcaf004691524
Sachin Gupta <sachin.14800@gmail.com>
2022-02-14 20:41:33 +0530

    code optimised using promises and return statement

--- Integrator.io/index.js
+++ Integrator.io/index.js
@@ -16,64 +16,111 @@ let options = {
     },
     body: connBody
 }
-
+let conn,exportId,importId,flowId,integrationId;
 rp(options)
 .then(body=>{
-    let conn1=JSON.parse(body);
+    conn=JSON.parse(body);
     let eBody=JSON.parse(exportBody);
-    eBody._connectionId=conn1._id;
-    console.log(`Connection created with id ${conn1._id}`);
+    eBody._connectionId=conn._id;
+    console.log(`Connection created with id ${conn._id}`);
     options.body=JSON.stringify(eBody);
     options.url='https://api.integrator.io/v1/exports';
-    rp(options)
-    .then(body=>{
-        let exportId=JSON.parse(body)._id;
-        console.log(`Export created with id ${exportId}`);
-        let iBody=JSON.parse(importBody);
-        iBody._connectionId=conn1._id;
-        options.body=JSON.stringify(iBody);
-        options.url='https://api.integrator.io/v1/imports';
-        rp(options)
-        .then(body=>{
-            let importId=JSON.parse(body)._id;
-            console.log(`Import created with id ${importId}`);
-            let fBody=JSON.parse(flowBody);
-            fBody.pageProcessors[0]._importId=importId;
-            fBody.pageGenerators[0]._exportId=exportId;
-            options.body=JSON.stringify(fBody);
-            options.url='https://api.integrator.io/v1/flows';
-            rp(options)
-            .then(body=>{
-                let flowId=JSON.parse(body)._id;
-                console.log(`Flow created with id ${flowId}`);
-                console.log(JSON.parse(body));
-                let integrateBody=JSON.parse(integrationBody);
-                integrateBody._registeredConnectionIds.push(flowId);
-                // console.log(integrateBody._registeredConnectionIds);
-                options.body=JSON.stringify(integrateBody);
-                rp(options)
-                .then(body=>{
-                    let integrationId=JSON.parse(body)._id;
-                    console.log(`Integration is created with id ${integrationId}`);
-                    console.log(JSON.parse(body));
-                })
-                .catch(err=>{
-                    console.log(`Error in creating the integration ${err}`);
-                })
-            })
-            .catch(err=>{
-                console.log(`Error in creating the flow ${err}`);
-            })
-        })
-        .catch(err=>{
-            console.log(`Error in creating import ${err}`);
-        })
-    })
-    .catch(err=>{
-        console.log(`Error in creating export ${err}`);
-    })
+    return rp(options);
+}).then(body=>{
+    exportId=JSON.parse(body)._id;
+    console.log(`Export created with id ${exportId}`);
+    let iBody=JSON.parse(importBody);
+    iBody._connectionId=conn._id;
+    options.body=JSON.stringify(iBody);
+    options.url='https://api.integrator.io/v1/imports';
+    return rp(options);
+})
+.then(body=>{
+    importId=JSON.parse(body)._id;
+    console.log(`Import created with id ${importId}`);
+    let fBody=JSON.parse(flowBody);
+    fBody.pageProcessors[0]._importId=importId;
+    fBody.pageGenerators[0]._exportId=exportId;
+    options.body=JSON.stringify(fBody);
+    options.url='https://api.integrator.io/v1/flows';
+    return rp(options);
+})
+.then(body=>{
+    flowId=JSON.parse(body)._id;
+    console.log(`Flow created with id ${flowId}`);
+    console.log(JSON.parse(body));
+    let integrateBody=JSON.parse(integrationBody);
+    integrateBody._registeredConnectionIds.push(flowId);
+    // console.log(integrateBody._registeredConnectionIds);
+    options.body=JSON.stringify(integrateBody);
+    return rp(options);
+})
+.then(body=>{
+    integrationId=JSON.parse(body)._id;
+    console.log(`Integration is created with id ${integrationId}`);
+    console.log(JSON.parse(body));
 })
 .catch(err=>{
-    console.log(`Error in creating the connection ${err}`);
-});
+    console.log(err);
+})
+
+// rp(options)
+// .then(body=>{
+//     let conn1=JSON.parse(body);
+//     let eBody=JSON.parse(exportBody);
+//     eBody._connectionId=conn1._id;
+//     console.log(`Connection created with id ${conn1._id}`);
+//     options.body=JSON.stringify(eBody);
+//     options.url='https://api.integrator.io/v1/exports';
+//     rp(options)
+//     .then(body=>{
+//         let exportId=JSON.parse(body)._id;
+//         console.log(`Export created with id ${exportId}`);
+//         let iBody=JSON.parse(importBody);
+//         iBody._connectionId=conn1._id;
+//         options.body=JSON.stringify(iBody);
+//         options.url='https://api.integrator.io/v1/imports';
+//         rp(options)
+//         .then(body=>{
+//             let importId=JSON.parse(body)._id;
+//             console.log(`Import created with id ${importId}`);
+//             let fBody=JSON.parse(flowBody);
+//             fBody.pageProcessors[0]._importId=importId;
+//             fBody.pageGenerators[0]._exportId=exportId;
+//             options.body=JSON.stringify(fBody);
+//             options.url='https://api.integrator.io/v1/flows';
+//             rp(options)
+//             .then(body=>{
+//                 let flowId=JSON.parse(body)._id;
+//                 console.log(`Flow created with id ${flowId}`);
+//                 console.log(JSON.parse(body));
+//                 let integrateBody=JSON.parse(integrationBody);
+//                 integrateBody._registeredConnectionIds.push(flowId);
+//                 // console.log(integrateBody._registeredConnectionIds);
+//                 options.body=JSON.stringify(integrateBody);
+//                 rp(options)
+//                 .then(body=>{
+//                     let integrationId=JSON.parse(body)._id;
+//                     console.log(`Integration is created with id ${integrationId}`);
+//                     console.log(JSON.parse(body));
+//                 })
+//                 .catch(err=>{
+//                     console.log(`Error in creating the integration ${err}`);
+//                 })
+//             })
+//             .catch(err=>{
+//                 console.log(`Error in creating the flow ${err}`);
+//             })
+//         })
+//         .catch(err=>{
+//             console.log(`Error in creating import ${err}`);
+//         })
+//     })
+//     .catch(err=>{
+//         console.log(`Error in creating export ${err}`);
+//     })
+// })
+// .catch(err=>{
+//     console.log(`Error in creating the connection ${err}`);
+// });
 
